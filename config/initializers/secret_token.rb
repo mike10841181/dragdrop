5ae3848b74664ee375269705fad656af40cd9e15
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2011-02-27 18:39:30 -0600

    primer commit de prueba

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dragdrop::Application.config.secret_token = '6de7a8971d817aaa6d38ea76056dd9f4eb59b6a30d070fc6cbab651194ce340958086c25fe3938b4650b7e0c03405ed78eb0870fafb0bfc3087e3ffbede2545a'
